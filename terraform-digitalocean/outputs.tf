3471fe03014e92e799ca837df1843ff5bb8efe56
Alan Tai <ayltai@gmail.com>
2019-10-14 14:35:29 +0800

    Try updating apt repositories before installing packages

--- terraform-digitalocean/outputs.tf
+++ terraform-digitalocean/outputs.tf
@@ -1,3 +1,7 @@
-output "public_id" {
+output "static_public_id" {
   value = digitalocean_floating_ip.hkvpn.ip_address
+}
+
+output "dynamic_public_ip" {
+  value = digitalocean_droplet.hkvpn.ipv4_address
 }
