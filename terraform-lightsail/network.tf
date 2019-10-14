e34300500ac74f4179f0f7c620a28316b8e6eb4a
Alan Tai <ayltai@gmail.com>
2019-10-14 17:01:43 +0800

    Use unique resource display names in Lightsail

--- terraform-lightsail/network.tf
+++ terraform-lightsail/network.tf
@@ -1,8 +1,8 @@
-resource "aws_lightsail_static_ip" "public" {
-  name = var.tag
+resource "aws_lightsail_static_ip" "hkvpn" {
+  name = "${var.tag}-ip"
 }
 
 resource "aws_lightsail_static_ip_attachment" "hkvpn" {
   instance_name  = aws_lightsail_instance.hkvpn.name
-  static_ip_name = aws_lightsail_static_ip.public.name
+  static_ip_name = aws_lightsail_static_ip.hkvpn.name
 }
--- terraform-lightsail/security.tf
+++ terraform-lightsail/security.tf
@@ -1,4 +1,4 @@
 resource "aws_lightsail_key_pair" "hkvpn" {
-  name       = var.tag
+  name       = "${var.tag}-key"
   public_key = file(var.public_key)
 }
