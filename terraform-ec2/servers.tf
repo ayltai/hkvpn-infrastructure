a5f2d401563fe96741535d7619ad6ef9b13774e1
Alan Tai <ayltai@gmail.com>
2020-01-14 15:32:43 +0800

    Fix incorrect public IP created while provisioning

--- terraform-ec2/servers.tf
+++ terraform-ec2/servers.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "hkvpn" {
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.hkvpn.id
-  associate_public_ip_address = "true"
-  key_name                    = aws_key_pair.hkvpn.key_name
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.hkvpn.id
+  key_name      = aws_key_pair.hkvpn.key_name
 
   vpc_security_group_ids = [
     aws_security_group.hkvpn.id,
